6c0258bf6903476a5efa4bf6cfe26c2e0af563a4
Blake <blake_gray1@baylor.edu>
2022-09-08 10:52:14 -0500

    latest Changes

--- employee_bonus.py
+++ employee_bonus.py
@@ -1,22 +1,28 @@
 def main():
     import csv
 
-    infile = ('EmployeePay.csv','r')
+    infile = open('EmployeePay.csv','r')
 
     csvfile = csv.reader(infile,delimiter=',')
+
     next(csvfile)
-
-
+    
+    index = 0
     for i in csvfile:
-        #infile.readlines()
-    #infile.readline()
-    #Repeating structure
         print('ID:',i[0])
-        print('Full name:',i[1])
-        print('Total salary:',i[3])
+        print('Full Name:',i[1]+' '+i[2])
+        print('Salary: ','$',i[3],sep='')
+        print('Bonus: ',float(i[4])*100,' %',sep='')
+
+        totalSal = int(i[3]) * (1 + float(i[4]))
+
+
+        print('Total Salary: ','$',format(totalSal,'.2f'),sep = '')
+        index +=1
 
         input()
-
-#def cal_total():
-    #Calculate total here, location 4 & 5 in the list
+        
+        
 main()
+
+
