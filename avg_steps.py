57e2468b20bc98002cfab8eea20b29426e36a08b
Blake <blake_gray1@baylor.edu>
2022-09-08 12:20:50 -0500

    Still not done with Avg_steps code

--- avg_steps.py
+++ avg_steps.py
@@ -10,20 +10,23 @@ outfile = open('avg_steps.csv','w')
 #writer = csv.writer(outfile,delimiter=',',newline='')
 
 print('Month | Average steps\n')
+
+num=1
+
 for num in range(1,13):
 
     steps = 0
     avg = 0 
+    days = 0
 
     infile = open('steps.csv','r')
     csvfile = csv.reader(infile,delimiter=',')
 
-
     for record in csvfile:
-
         
         if str(i) == record[0]:  #I need it to add all the steps and then divide it by the total number of days
             steps = steps + int(record[1])
+            days = days + record[0]
     outfile.write()
     infile.close()
 
