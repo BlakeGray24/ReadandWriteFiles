1e45352b33546b7e4f3f55626f8cd50ddd54c4c3
Blake <blake_gray1@baylor.edu>
2022-09-08 20:20:47 -0500

    Getting Closer, program breaks after first loop

--- avg_steps.py
+++ avg_steps.py
@@ -3,13 +3,14 @@ import csv
 #infile = open('steps.csv','r')
 #csvfile = csv.reader(infile,delimiter=',')
 
-months = ['Janurary','Feburary','March','April','May','June','July','August','September','October','November','December']
+months = ['0','Janurary','Feburary','March','April','May','June','July','August','September','October','November','December']
 
-outfile = open('avg_steps.csv','w')
-
-writer = csv.writer(outfile,delimiter=',',newline='')
+outfile = open('avg_steps.csv','w') 
+#data_frame=  
+#writer = csv.writer(outfile,delimiter=',',newline='')
 
 print('Month | Average steps\n')
+outfile.write('Month | Average steps\n')
 
 num=1
 
@@ -20,17 +21,43 @@ for num in range(1,13):
     days = 0
 
     infile = open('steps.csv','r')
-    csvfile = csv.reader(infile,delimiter=',')
+
+    csvfile = csv.reader(infile, delimiter=',')
+
+    next(csvfile)
+    i=1
 
     for record in csvfile:
         
         if str(i) == record[0]:  #I need it to add all the steps and then divide it by the total number of days
-            steps = steps + int(record[1])
-            days = days + 1
-            avg = (steps / days)
-            
-    outfile.write()
-    infile.close()
+            steps += int(record[1])
+            days  += 1
 
+            print(steps)
+            print(days)
+            print(months[i])
+
+            
+
+        else:
+            avg = float(steps/days)
+
+            print('%.0f'% avg)
+
+            avg = str(avg)
+
+
+            outfile.write(months[i] + ',' + avg)
+            
+            
+            steps = record[i]
+            days = i
+
+            #outfile.write(months[i],avg)
+            # 
+    infile.close()
 outfile.close()
+
+
+
 
